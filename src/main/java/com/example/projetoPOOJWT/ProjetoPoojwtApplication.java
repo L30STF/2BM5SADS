e7f01039618f531b76f3264a59b5a235afd17fc2
L30STF <166737740+L30STF@users.noreply.github.com>
2025-05-15 20:18:35 -0300

    first commit

---
+++ src/main/java/com/example/projetoPOOJWT/ProjetoPoojwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoPOOJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPoojwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPoojwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetoPOOJWT/ProjetoPoojwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoPOOJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPoojwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
